86cf4926dcf00ed4ed146146d119822d2c70f85b
istkzafar <istkzafar.com>
2020-12-26 12:25:13 +0530

    Updates

---
+++ precache-manifest.3f1dcc3f31f0a4762f8091ba0a11f626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec303f1048b88ef26533c5936d0d2023",
+    "url": "/homeShopping/index.html"
+  },
+  {
+    "revision": "4fa76174c114f0bc476f",
+    "url": "/homeShopping/static/css/main.9ad47c6c.chunk.css"
+  },
+  {
+    "revision": "4b6c937a7503342c6dc3",
+    "url": "/homeShopping/static/js/2.b40003cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homeShopping/static/js/2.b40003cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa76174c114f0bc476f",
+    "url": "/homeShopping/static/js/main.df6192f4.chunk.js"
+  },
+  {
+    "revision": "2945473727091a83337f",
+    "url": "/homeShopping/static/js/runtime-main.27039b98.js"
+  }
+]);
